fc12124967aeca538e5524d7a2217cb0b1c26eba
Justin Howard <50159035+zorro1rr@users.noreply.github.com>
2022-04-12 17:51:54 -0600

    work in progress

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useQuery } from 'react-query';
 import useBooksApi from '../hooks/useBooksApi';
 import { saveAuthToken } from '../utils/local-storage';
 import Book from '../components/Book';
@@ -8,8 +9,8 @@ import Header from '../components/Header';
 export default function DashboardPage() {
   const { getAccessTokenSilently } = useAuth0();
   const { getAllBooks } = useBooksApi();
-  const [books, setBooks] = useState(null);
-  const [loading, setLoading] = useState(true);
+  // const [books, setBooks] = useState(null);
+  // const [loading, setLoading] = useState(true);
 
   useEffect(async () => {
     const getAccessToken = async () => {
@@ -23,38 +24,30 @@ export default function DashboardPage() {
         console.log(e.message);
       }
     };
-
-    const getAllBooksEffect = async () => {
-      try {
-        const data = await getAllBooks();
-        setBooks(data);
-      } catch (e) {
-        console.log(e.message);
-      }
-    };
     await getAccessToken();
-    getAllBooksEffect();
+    // getAllBooksEffect();
   }, []);
 
-  useEffect(() => {
-    if (books) {
-      setLoading(false);
-    }
-  }, [books]);
+  const { data: books, isLoading, isError } = useQuery('getBooks', getAllBooks);
+  console.log('books', books);
+  // useEffect(() => {
+  //   if (books) {
+  //     setLoading(false);
+  //   }
+  // }, [books]);
 
-  let booklist;
-  if (loading) {
-    booklist = null;
-  } else {
-    booklist = (
-      <div className="grid grid-cols-1 pb-6 mx-6 gap-x-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {books.map((book) => (
-          <Book book={book} key={book.book_id} />
-        ))}
-      </div>
-    );
-  }
-
+  // let booklist;
+  // if (isLoading) {
+  //   booklist = null;
+  // } else {
+  //   booklist = (
+  //     <div className="grid grid-cols-1 pb-6 mx-6 gap-x-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+  //       {books.map((book) => (
+  //         <Book book={book} key={book.book_id} />
+  //       ))}
+  //     </div>
+  //   );
+  // }
   return (
     <section className="sm:grid grid-cols-layout grid-rows-layout">
       <Header />
@@ -62,7 +55,15 @@ export default function DashboardPage() {
         <div>
           <h2 className="px-5 pt-5 text-3xl font-bold text-gray-900">Books</h2>
         </div>
-        {booklist}
+        {isLoading && <h2>Loading Books...</h2>}
+        {isError && <h2>Error Fetching Books</h2>}
+        {books && (
+          <div className="grid grid-cols-1 pb-6 mx-6 gap-x-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {books.map((book) => (
+              <Book book={book} key={book.book_id} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
